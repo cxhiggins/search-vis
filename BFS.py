10d3a8b16c7a01f5a672e118799f9e44893c182f
Cara Higgins <52946616+cxhiggins@users.noreply.github.com>
2020-04-02 22:13:31 +0100

    BFS initial commit

--- BFS.py
+++ BFS.py
@@ -3,45 +3,63 @@ import matplotlib.pyplot as plt
 import queue
 import time
 
-plt.ion()
+def gen_bfs_graph():
 
-G = nx.erdos_renyi_graph(10, 0.15, directed=True)
-pos = nx.circular_layout(G)
-##pos = nx.kamada_kawai_layout(G)
-l = len(list(G)) # max val for d[u]
-d = []
-p = []
-x = []
-i = 0
+    plt.ion()
+    plt.figure(figsize=(5,5))
 
-for node in G:
-    d.append(l)
-    p.append(-1)
-    x.append([])
-d[0] = 0
-p[0] = -1
-
-nx.draw(G, node_color='#d1f1ff', pos=pos, with_labels=True)
-plt.savefig("graph"+str(i)+".png")
-
-q = queue.Queue(0)
-q.put(0) # start from node 0
-while not q.empty():
-    u = q.get()
-    x[d[u]].append(u)
+    node_size = 1000
+    G = nx.erdos_renyi_graph(10, 0.15, directed=True)
+    pos = nx.circular_layout(G)
+    # Note: many other types of layouts are supported, including
+    #       kamada_kawai_layout
     
-    for v in G[u]:
-        if d[v] == l:
-            d[v] = d[u] + 1
-            p[v] = u
-            q.put(v)
+    d = [] # d[v] = num edges in shortest path from source to v
+    p = [] # p[v] = predecessor in shortest path from source to v
+    x = [] # x[v] = [u | u in G.nodes, d[u] = v]
+    l = len(list(G)) # num nodes in G
+    i = 0  # frame number
+    
+    def update_graph(color, nodes):
+        nx.draw_networkx_nodes(G, nodelist=nodes, node_color=color, node_size=node_size, pos=pos, with_labels=True)
 
-lastnodes = []
-for nodes in x:
-    if not nodes == []:
-        i+=1
-        nx.draw_networkx_nodes(G, nodelist=nodes, node_color='#69d1ff', pos=pos, with_labels=True)
+    def save_image():
         plt.savefig("graph"+str(i)+".png")
-    lastnodes = nodes
-    nx.draw_networkx_nodes(G, nodelist=lastnodes, node_color='#1e7da5', pos=pos, with_labels=True)
-plt.savefig("graph"+str(i)+".png")
+
+    for node in G:
+        d.append(l)
+        # max len of shortest path in G is l-1
+        # d[v] = l means v isn't reachable from source
+        p.append(-1)
+        # -1 is used to specify that a node doesn't have a predecessor
+        x.append([])
+        
+    # 0 is the specified starting node
+    d[0] = 0     
+    nx.draw(G, node_color='#d1f1ff', node_size=node_size, pos=pos, with_labels=True)
+    save_image()  
+
+    q = queue.Queue(0)
+    q.put(0) # start from node 0
+    while not q.empty():
+        u = q.get()
+        x[d[u]].append(u)
+        
+        for v in G[u]:
+            if d[v] == l:
+                d[v] = d[u] + 1
+                p[v] = u
+                q.put(v)
+
+    lastnodes = []
+    for nodes in x:
+        if not nodes == []:
+            i+=1
+            update_graph('#69d1ff', nodes)
+            save_image()
+        lastnodes = nodes
+        update_graph('#1e7da5', lastnodes)
+    save_image()  
+
+def main():
+    gen_bfs_graph()
